23459ee7806a48860e2728417b51fb9a8478dd40
Kristoffer <kriss070697@gmail.com>
2021-07-02 06:32:53 +0200

    AppDev v2.1 - Build 15
    - Added Menu, no interaction.
    - Added footer label containing author and version.
    - Scrollbar is still not gone.

---
+++ appdev/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+
+namespace appdev
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const double prog_version = 2.1;
+        const string author = "krissy";
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Reflection.Assembly executingAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(executingAssembly.Location);
+            var major = fileVersionInfo.FileMajorPart;
+            var minor = fileVersionInfo.FileMinorPart;
+            var build = fileVersionInfo.FileBuildPart;
+
+
+            lblVersion.Content = "Developed by " + author + " - " + "v" + major + "." + minor + " B" + build;
+        }
+
+
+    }
+}
