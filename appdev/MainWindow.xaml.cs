ffae781783e3c35396e13a1d2027320c3975394f
Kristoffer <kriss070697@gmail.com>
2021-07-03 02:50:55 +0200

    rework

--- appdev/MainWindow.xaml.cs
+++ appdev/MainWindow.xaml.cs
@@ -27,7 +27,24 @@ namespace appdev
         const double prog_version = 2.1;
         const string author = "krissy";
 
+        const int default_width = 375;
+        const int default_height = 812;
 
+        const int width_iphonex = 375;
+        const int height_iphonex = 862;
+        
+
+        const int width_samsungs9 = 360;
+        const int height_samsungs9 = 790;
+
+        const int width_samsungs10 = 360;
+        const int height_samsungs10 = 810;
+
+        const int width_iphone11 = 276;
+        const int height_iphone11 = 648;
+
+        const int width_p30 = 360;
+        const int height_p30 = 830;
 
 
         public MainWindow()
@@ -39,6 +56,9 @@ namespace appdev
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            ScrollViewer s = new ScrollViewer();
+            s.VerticalScrollBarVisibility = ScrollBarVisibility.Hidden;
+
             System.Reflection.Assembly executingAssembly = System.Reflection.Assembly.GetExecutingAssembly();
             var fileVersionInfo = FileVersionInfo.GetVersionInfo(executingAssembly.Location);
             var major = fileVersionInfo.FileMajorPart;
@@ -49,6 +69,39 @@ namespace appdev
             lblVersion.Content = "Developed by " + author + " - " + "v" + major + "." + minor + " B" + build;
         }
 
+        private void webBrowser_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+            e.Handled = true;
+        }
 
+        private void iphonex_Click(object sender, RoutedEventArgs e)
+        {
+            Width = default_width;
+            Height = height_iphonex;
+        }
+
+        private void samsungs9_Click(object sender, RoutedEventArgs e)
+        {
+            Width = width_samsungs9;
+            Height = height_samsungs9;
+        }
+
+        private void samsungs10_Click(object sender, RoutedEventArgs e)
+        {
+            Width = width_samsungs10;
+            Height = height_samsungs10;
+        }
+
+        private void iphone11_Click(object sender, RoutedEventArgs e)
+        {
+            Width = width_iphone11;
+            Height = height_iphone11;
+        }
+
+        private void huaweip30_Click(object sender, RoutedEventArgs e)
+        {
+            Width = width_p30;
+            Height = height_p30;
+        }
     }
 }
--- appdev/Properties/AssemblyInfo.cs
+++ appdev/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("appdev")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kristoffer")]
 [assembly: AssemblyProduct("appdev")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.15.0")]
-[assembly: AssemblyFileVersion("2.1.15.0")]
+[assembly: AssemblyVersion("5.2.5.0")]
+[assembly: AssemblyFileVersion("5.2.5.0")]
