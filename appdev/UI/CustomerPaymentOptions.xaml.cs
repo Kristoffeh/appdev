754c43a709ee63458187184db8c3c21c18c59b4e
Kristoffer <kriss070697@gmail.com>
2021-07-05 07:52:51 +0200

    more stuff
    - Added CustomerPaymentOptions where users can manage their cards.
    - Added CustomerPortal where users can manage subscriptions.
    - SelectAccount.xaml allows users to switch to a different account if they have the Stripe ID.
    - Account balance is now shown as credit in CustomerPaymentOptions.
    - User will be told if they haven't added any cards in CustomerPaymentOptions.
    - Improved code.
    - Fixed bugs.
    - Added more feedback flexibility.

---
+++ appdev/UI/CustomerPaymentOptions.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace appdev.UI
+{
+    /// <summary>
+    /// Interaction logic for CustomerPaymentOptions.xaml
+    /// </summary>
+    public partial class CustomerPaymentOptions : Window
+    {
+        public CustomerPaymentOptions()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StripeConfiguration.ApiKey = "sk_test_sUA4LoMrFUHdtNKDr311Q3hC00g25NqDKt";
+
+            var servicea = new CustomerService();
+            var r_customer = servicea.Get(Properties.Settings.Default.stripeUserID);
+
+            // Remove last 2 digits
+            long rm1 = r_customer.Balance / 10;
+            long rm2 = rm1 / 10;
+
+            // get last two digits
+            long lastTwo = r_customer.Balance % 100;
+
+            string a = rm2.ToString();
+            string bstring = a.Replace("-", string.Empty);
+
+
+
+            lblCreditValue.Content = "$" + String.Format("{0:0.00}", bstring);
+
+            // Cards object (list all cards)
+            var service = new CardService();
+            var options = new CardListOptions
+            {
+                Limit = 3,
+            };
+            var cards = service.List(Properties.Settings.Default.stripeUserID, options);
+
+            // Customer object
+            var serviceb = new CustomerService();
+            var c = serviceb.Get(Properties.Settings.Default.stripeUserID);
+
+
+
+            // MessageBox.Show("" + cards ,"");
+
+            if (cards.Count() == 0)
+            {
+                lblNoCards.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                cbListCards.Content = "";
+            }
+        }
+    }
+}
--- appdev/UI/CustomerPortal.xaml.cs
+++ appdev/UI/CustomerPortal.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Globalization;
+using System.Threading;
 
 namespace appdev.UI
 {
@@ -29,18 +31,41 @@ namespace appdev.UI
         {
             try
             {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+
+                CultureInfo ci = new CultureInfo("en-US");
+                CultureInfo.DefaultThreadCurrentCulture = ci;
+                CultureInfo.DefaultThreadCurrentUICulture = ci;
+
                 // Retrieve product
                 StripeConfiguration.ApiKey = "sk_test_sUA4LoMrFUHdtNKDr311Q3hC00g25NqDKt";
 
                 var service = new ProductService();
                 var service_output = service.Get("prod_JmwBudJpNuZxXC");
 
-
-                btnUserID.Content = Properties.Settings.Default.stripeUserID + "";
                 
+                btnUserID.Content = Properties.Settings.Default.stripeUserID + "";
+
                 // txtLogs.Text += "User ID: " + Properties.Settings.Default.stripeUserID + "\r\n \r\n";
                 // txtLogs.Text += service_output;
                 // StripeCreateSubscription();
+
+
+                // Retrieve name from Stripe
+                var service_name = new CustomerService();
+                var customer_name = service_name.Get(Properties.Settings.Default.stripeUserID);
+
+                // Subscription service
+                var servicea = new SubscriptionItemService();
+                var asd = servicea.Get("si_JnHj9eFeVEqXGV");
+
+                btnName.Content = customer_name.Name;
+                lblSubscribePrice.Text = "Subscribe for $" + String.Format("{0:0.00}", 4.99) + " /month";
+
+
+
+
             }
             catch (Exception x)
             {
@@ -67,6 +92,47 @@ namespace appdev.UI
             };
             var service = new SubscriptionService();
             service.Create(options);
+        }
+
+        private void btnSubscribe_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StripeConfiguration.ApiKey = "sk_test_sUA4LoMrFUHdtNKDr311Q3hC00g25NqDKt";
+
+                var options = new SubscriptionCreateOptions
+                {
+                    Customer = Properties.Settings.Default.stripeUserID,
+                    Items = new List<SubscriptionItemOptions>
+
+                {
+                    new SubscriptionItemOptions
+                    {
+                      Price = "price_1J9MIvDjGfNenHs5hp9wrYt3",
+                    },
+                },
+                };
+                var service = new SubscriptionService();
+                var s = service.Create(options);
+
+                // Status handling
+                /*if (s.Status == "")
+                {
+
+                }*/
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex.Message,"");
+                throw;
+            }
+        }
+
+        private void CustomerPaymentOptions_Click(object sender, RoutedEventArgs e)
+        {
+            CustomerPaymentOptions cpo = new CustomerPaymentOptions();
+            cpo.Show();
         }
     }
 }
